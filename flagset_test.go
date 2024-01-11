9545474b311a57f0c2b428bac86ef91bac799dd1
heartwilltell <heartwilltell@gmail.com>
2024-01-11 10:50:08 +0100

    Add tests

--- flagset_test.go
+++ flagset_test.go
@@ -1,1 +1,38 @@
 package scotty
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_tern(t *testing.T) {
+	type tcase[T any] struct {
+		cond       bool
+		t, f, want T
+	}
+
+	tests := map[string]tcase[string]{
+		"true": {
+			cond: true,
+			t:    "true",
+			f:    "false",
+			want: "true",
+		},
+
+		"false": {
+			cond: false,
+			t:    "true",
+			f:    "false",
+			want: "false",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := tern(tc.cond, tc.t, tc.f)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("tern got := %v want := %v", got, tc.want)
+			}
+		})
+	}
+}
