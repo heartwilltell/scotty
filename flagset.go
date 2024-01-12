cc6badd729e94ede7176eefd74da8c67f8df5fac
heartwilltell <heartwilltell@gmail.com>
2024-01-12 11:21:48 +0100

    Add tests

--- flagset.go
+++ flagset.go
@@ -2,6 +2,7 @@ package scotty
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 	"time"
@@ -16,7 +17,7 @@ type FlagSet struct{ *flag.FlagSet }
 // Flag has priority over environment variable. If flag not set the environment variable value will be used.
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) StringVarE(p *string, flagName, envName, value, usage string) {
-	f.FlagSet.StringVar(p, flagName, tern[string](os.Getenv(envName) == "", os.Getenv(envName), value), usage)
+	f.FlagSet.StringVar(p, flagName, tern[string](os.Getenv(envName) != "", os.Getenv(envName), value), usage)
 }
 
 // BoolVarE defines a bool flag and environment variable with specified name, default value, and usage string.
@@ -25,7 +26,7 @@ func (f *FlagSet) StringVarE(p *string, flagName, envName, value, usage string) 
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) BoolVarE(p *bool, flagName, envName string, value bool, usage string) {
 	parsed, err := strconv.ParseBool(os.Getenv(envName))
-	f.FlagSet.BoolVar(p, flagName, tern[bool](err != nil, parsed, value), usage)
+	f.FlagSet.BoolVar(p, flagName, tern[bool](err == nil, parsed, value), usage)
 }
 
 // IntVarE defines an int flag and environment variable with specified name, default value, and usage string.
@@ -34,7 +35,7 @@ func (f *FlagSet) BoolVarE(p *bool, flagName, envName string, value bool, usage 
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) IntVarE(p *int, flagName, envName string, value int, usage string) {
 	parsed, err := strconv.Atoi(os.Getenv(envName))
-	f.FlagSet.IntVar(p, flagName, tern[int](err != nil, parsed, value), usage)
+	f.FlagSet.IntVar(p, flagName, tern[int](err == nil, parsed, value), usage)
 }
 
 // Int64VarE defines an int64 flag and environment variable with specified name, default value, and usage string.
@@ -43,7 +44,7 @@ func (f *FlagSet) IntVarE(p *int, flagName, envName string, value int, usage str
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) Int64VarE(p *int64, flagName, envName string, value int64, usage string) {
 	parsed, err := strconv.Atoi(os.Getenv(envName))
-	f.FlagSet.Int64Var(p, flagName, tern[int64](err != nil, int64(parsed), value), usage)
+	f.FlagSet.Int64Var(p, flagName, tern[int64](err == nil, int64(parsed), value), usage)
 }
 
 // Float64VarE defines a float64 flag and environment variable with specified name, default value, and usage string.
@@ -52,7 +53,7 @@ func (f *FlagSet) Int64VarE(p *int64, flagName, envName string, value int64, usa
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) Float64VarE(p *float64, flagName, envName string, value float64, usage string) {
 	parsed, err := strconv.ParseFloat(os.Getenv(envName), 64)
-	f.FlagSet.Float64Var(p, flagName, tern[float64](err != nil, parsed, value), usage)
+	f.FlagSet.Float64Var(p, flagName, tern[float64](err == nil, parsed, value), usage)
 }
 
 // UintVarE defines an uint flag and environment variable with specified name, default value, and usage string.
@@ -61,7 +62,7 @@ func (f *FlagSet) Float64VarE(p *float64, flagName, envName string, value float6
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) UintVarE(p *uint, flagName, envName string, value uint, usage string) {
 	parsed, err := strconv.Atoi(os.Getenv(envName))
-	f.FlagSet.UintVar(p, flagName, tern[uint](err != nil, uint(parsed), value), usage)
+	f.FlagSet.UintVar(p, flagName, tern[uint](err == nil, uint(parsed), value), usage)
 }
 
 // Uint64VarE defines an uint64 flag and environment variable with specified name, default value, and usage string.
@@ -70,7 +71,7 @@ func (f *FlagSet) UintVarE(p *uint, flagName, envName string, value uint, usage 
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) Uint64VarE(p *uint64, flagName, envName string, value uint64, usage string) {
 	parsed, err := strconv.Atoi(os.Getenv(envName))
-	f.FlagSet.Uint64Var(p, flagName, tern[uint64](err != nil, uint64(parsed), value), usage)
+	f.FlagSet.Uint64Var(p, flagName, tern[uint64](err == nil, uint64(parsed), value), usage)
 }
 
 // DurationVarE defines a time.Duration flag and environment variable with specified name, default value, and usage string.
@@ -79,7 +80,8 @@ func (f *FlagSet) Uint64VarE(p *uint64, flagName, envName string, value uint64, 
 // If the value of environment variable can't be parsed to destination type the default value will be used.
 func (f *FlagSet) DurationVarE(p *time.Duration, flagName, envName string, value time.Duration, usage string) {
 	parsed, err := time.ParseDuration(os.Getenv(envName))
-	f.FlagSet.DurationVar(p, flagName, tern[time.Duration](err != nil, parsed, value), usage)
+	fmt.Println(parsed, err, tern[time.Duration](err == nil, parsed, value))
+	f.FlagSet.DurationVar(p, flagName, tern[time.Duration](err == nil, parsed, value), usage)
 }
 
 //nolint:revive // flag-parameter is ok here.
--- flagset_test.go
+++ flagset_test.go
@@ -1,9 +1,617 @@
 package scotty
 
 import (
+	"flag"
 	"reflect"
 	"testing"
+	"time"
 )
+
+func TestFlagSet_DurationVarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want time.Duration
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[time.Duration]{
+		"Flag": {
+			want: 11 * time.Second,
+			before: func(t *testing.T, f *FlagSet, got *time.Duration) {
+				t.Helper()
+
+				f.DurationVarE(got, "f1", "TEST_E1", 10*time.Second, "")
+			},
+			args: []string{"-f1=11s"},
+		},
+
+		"Env": {
+			want: 11 * time.Second,
+			before: func(t *testing.T, f *FlagSet, got *time.Duration) {
+				t.Helper()
+				t.Setenv("TEST_E1", "11s")
+				f.DurationVarE(got, "f1", "TEST_E1", 10*time.Second, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 10 * time.Second,
+			before: func(t *testing.T, f *FlagSet, got *time.Duration) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.DurationVarE(got, "f1", "TEST_E1", 10*time.Second, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 11 * time.Second,
+			before: func(t *testing.T, f *FlagSet, got *time.Duration) {
+				t.Helper()
+				t.Setenv("TEST_E1", "12s")
+				f.DurationVarE(got, "f1", "TEST_E1", 10*time.Second, "")
+			},
+			args: []string{"-f1=11s"},
+		},
+
+		"Default": {
+			want: 10 * time.Second,
+			before: func(t *testing.T, f *FlagSet, got *time.Duration) {
+				t.Helper()
+				f.DurationVarE(got, "f1", "TEST_E1", 10*time.Second, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_BoolVarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want bool
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[bool]{
+		"Flag": {
+			want: true,
+			before: func(t *testing.T, f *FlagSet, got *bool) {
+				t.Helper()
+
+				f.BoolVarE(got, "f1", "TEST_E1", false, "")
+			},
+			args: []string{"-f1"},
+		},
+
+		"Env": {
+			want: true,
+			before: func(t *testing.T, f *FlagSet, got *bool) {
+				t.Helper()
+				t.Setenv("TEST_E1", "true")
+				f.BoolVarE(got, "f1", "TEST_E1", false, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: false,
+			before: func(t *testing.T, f *FlagSet, got *bool) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.BoolVarE(got, "f1", "TEST_E1", false, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: true,
+			before: func(t *testing.T, f *FlagSet, got *bool) {
+				t.Helper()
+				t.Setenv("TEST_E1", "false")
+				f.BoolVarE(got, "f1", "TEST_E1", false, "")
+			},
+			args: []string{"-f1"},
+		},
+
+		"Default": {
+			want: false,
+			before: func(t *testing.T, f *FlagSet, got *bool) {
+				t.Helper()
+				f.BoolVarE(got, "f1", "TEST_E1", false, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_Float64VarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want float64
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[float64]{
+		"Flag": {
+			want: 10.2,
+			before: func(t *testing.T, f *FlagSet, got *float64) {
+				t.Helper()
+
+				f.Float64VarE(got, "f1", "TEST_E1", 10.1, "")
+			},
+			args: []string{"-f1=10.2"},
+		},
+
+		"Env": {
+			want: 10.2,
+			before: func(t *testing.T, f *FlagSet, got *float64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10.2")
+				f.Float64VarE(got, "f1", "TEST_E1", 10.1, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 10.1,
+			before: func(t *testing.T, f *FlagSet, got *float64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.Float64VarE(got, "f1", "TEST_E1", 10.1, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 10.2,
+			before: func(t *testing.T, f *FlagSet, got *float64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10.3")
+				f.Float64VarE(got, "f1", "TEST_E1", 10.1, "")
+			},
+			args: []string{"-f1=10.2"},
+		},
+
+		"Default": {
+			want: 10.1,
+			before: func(t *testing.T, f *FlagSet, got *float64) {
+				t.Helper()
+				f.Float64VarE(got, "f1", "TEST_E1", 10.1, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_Int64VarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want int64
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[int64]{
+		"Flag": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int64) {
+				t.Helper()
+
+				f.Int64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Env": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10")
+				f.Int64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *int64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.Int64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "12")
+				f.Int64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Default": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *int64) {
+				t.Helper()
+				f.Int64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_IntVarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want int
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[int]{
+		"Flag": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int) {
+				t.Helper()
+
+				f.IntVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Env": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10")
+				f.IntVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *int) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.IntVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *int) {
+				t.Helper()
+				t.Setenv("TEST_E1", "12")
+				f.IntVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Default": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *int) {
+				t.Helper()
+				f.IntVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_StringVarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want string
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[string]{
+		"Flag": {
+			want: "plainq",
+			before: func(t *testing.T, f *FlagSet, got *string) {
+				t.Helper()
+
+				f.StringVarE(got, "f1", "TEST_E1", "", "")
+			},
+			args: []string{"-f1=plainq"},
+		},
+
+		"Env": {
+			want: "plainq",
+			before: func(t *testing.T, f *FlagSet, got *string) {
+				t.Helper()
+				t.Setenv("TEST_E1", "plainq")
+				f.StringVarE(got, "f1", "TEST_E1", "", "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: "plainq",
+			before: func(t *testing.T, f *FlagSet, got *string) {
+				t.Helper()
+				t.Setenv("TEST_E1", "plainq2")
+				f.StringVarE(got, "f1", "TEST_E1", "", "")
+			},
+			args: []string{"-f1=plainq"},
+		},
+
+		"Default": {
+			want: "plainq",
+			before: func(t *testing.T, f *FlagSet, got *string) {
+				t.Helper()
+				f.StringVarE(got, "f1", "TEST_E1", "plainq", "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_Uint64VarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want uint64
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[uint64]{
+		"Flag": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint64) {
+				t.Helper()
+
+				f.Uint64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Env": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10")
+				f.Uint64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *uint64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.Uint64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint64) {
+				t.Helper()
+				t.Setenv("TEST_E1", "12")
+				f.Uint64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Default": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *uint64) {
+				t.Helper()
+				f.Uint64VarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
+
+func TestFlagSet_UintVarE(t *testing.T) {
+	type tcase[T any] struct {
+		got, want uint
+		before    func(t *testing.T, f *FlagSet, got *T)
+		args      []string
+	}
+
+	tests := map[string]tcase[uint]{
+		"Flag": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint) {
+				t.Helper()
+
+				f.UintVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Env": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint) {
+				t.Helper()
+				t.Setenv("TEST_E1", "10")
+				f.UintVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"InvalidEnv": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *uint) {
+				t.Helper()
+				t.Setenv("TEST_E1", "lalala")
+				f.UintVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+
+		"BothSet": {
+			want: 10,
+			before: func(t *testing.T, f *FlagSet, got *uint) {
+				t.Helper()
+				t.Setenv("TEST_E1", "12")
+				f.UintVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{"-f1=10"},
+		},
+
+		"Default": {
+			want: 11,
+			before: func(t *testing.T, f *FlagSet, got *uint) {
+				t.Helper()
+				f.UintVarE(got, "f1", "TEST_E1", 11, "")
+			},
+			args: []string{},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			f := &FlagSet{
+				FlagSet: flag.NewFlagSet("test", flag.ExitOnError),
+			}
+
+			tc.before(t, f, &tc.got)
+
+			if err := f.Parse(tc.args); err != nil {
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(tc.want, tc.got) {
+				t.Errorf("want := %v, got := %v", tc.want, tc.got)
+			}
+		})
+	}
+}
 
 func Test_tern(t *testing.T) {
 	type tcase[T any] struct {
